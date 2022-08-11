61167cc3d005f5292f575b9d777a63ad56c525ba
andrewjlock <andrew.john.lock@gmail.com>
2022-08-11 10:48:25 +1000

    read_file now PEP8 compliant

--- src/motiontrack/read_data.py
+++ src/motiontrack/read_data.py
@@ -1,69 +1,105 @@
 #!/usr/bin/python3.8
-import os
-import sys
+
+""" Class and function to read blob data from file
+"""
+
+from typing import List
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 
-class Blobs2D:
-    # A set of 2D blob data from image data
-    def __init__(self,p,D):
-        self.p = p # x,y coordinates
-        self.D = D # Diamters
-        self.n = p.shape[1]
-        # Add new data if used later
+class BlobsFrame:
+    """ Class to represent 2D blob data found by image recognition
+    TODO: better structure for this using dictionaries.
+    """
+    def __init__(self, points: np.array, diameters: np.array):
+        self.points = points # x,y coordinates
+        self.diameters = diameters # Diameters
+        self.n_blobs = points.shape[1]
 
-    def remove_blob(self,i):
-        self.p = np.delete(self.p,i,axis=1)
-        self.D = np.delete(self.D,i)
-        self.n -= 1
+    def remove_blob(self, i: int):
+        """
+        Remove blob point from frame
+
+        Parameters
+        ----------
+        i : int
+            index of blob to remove
+        """
+        self.points = np.delete(self.points, i, axis=1)
+        self.diameters = np.delete(self.diameters, i)
+        self.n_blobs -= 1
 
     def plot_frame(self):
-        fig,ax = plt.subplots()
-        ax.plot(self.p[0],self.p[1],'o')
+        """
+        Plot the 2D blob frame
+
+        """
+        fig, ax = plt.subplots()
+        ax.plot(self.points[0], self.points[1], 'o')
         plt.show()
 
-class fileData:
-    def __init__(self,filename):
-        fileData = pd.read_csv(filename,
+class BlobFile:
+    """ Stores all blob data from a file and processes it into frames
+    """
+    def __init__(self, filename: str):
+        blob_data = pd.read_csv(filename,
                                header=0,
                                sep=" ",
-                               index_col=False) 
+                               index_col=False)
 
         # Find the names of headers for the important parameters (search for part of string)
         #TODO: Add more blob details if we have them
-        self.frame_header = list(filter(lambda x: "Image" in x, fileData.keys()))[0]
-        self.x_header = list(filter(lambda x: "xCord" in x, fileData.keys()))[0]
-        self.y_header = list(filter(lambda x: "yCord" in x, fileData.keys()))[0]
-        self.size_header = list(filter(lambda x: "size" in x, fileData.keys()))[0]
+        self.frame_header = list(filter(lambda x: "Image" in x, blob_data.keys()))[0]
+        self.x_header = list(filter(lambda x: "xCord" in x, blob_data.keys()))[0]
+        self.y_header = list(filter(lambda x: "yCord" in x, blob_data.keys()))[0]
+        self.size_header = list(filter(lambda x: "size" in x, blob_data.keys()))[0]
 
         self.name = filename.split("/")[-1].split(".")[0]
-        self.data = fileData
-        self.frames = fileData[self.frame_header]
-        self.X = fileData[self.x_header]
-        self.Y = fileData[self.y_header]
-        self.D = fileData[self.size_header]
-        self.frame_start = np.min(fileData[self.frame_header])
-        self.frame_end = np.max(fileData[self.frame_header])
+        self.data = blob_data
+        self.frames = blob_data[self.frame_header]
+        self.X = blob_data[self.x_header]
+        self.Y = blob_data[self.y_header]
+        self.D = blob_data[self.size_header]
+        self.frame_start = np.min(blob_data[self.frame_header])
+        self.frame_end = np.max(blob_data[self.frame_header])
 
+def read_blob_data(filenames: List[str]) -> dict:
+    """
+    Reads one or more files containing 2D blob data, and produces a dictionary of
+    BlobsFrames
 
-def read_files(filenames):
-    # Iterates through frames from each 
-    files = []
+    A BlobFrame instance is created for each video frame.
+    All frames are stored within a BlobData object.
+
+    Parameters
+    ----------
+    filenames : List[str]
+        A list of filenames containing the blob data
+
+    Returns
+    -------
+    dict
+        A dictionary with structure:
+        { <frame no.> : {<filename> : <BlobsFrame> } }
+
+    """
+
+    blob_files = []
     for filename in filenames:
-        files.append(fileData(filename))
+        blob_files.append(BlobFile(filename))
 
-    frame_start = np.min([f.frame_start for f in files])
-    frame_end = np.max([f.frame_end for f in files])
+    frame_start = np.min([f.frame_start for f in blob_files])
+    frame_end = np.max([f.frame_end for f in blob_files])
 
-    frameData = {frame:{} for frame in range(frame_start,frame_end)} # Dict of files
+    frame_dict = {frame:{} for frame in range(frame_start,frame_end)} # Dict of files
 
-    for frame in range(frame_start,frame_end):
-        for file in files:
-            if frame in file.data[file.frame_header]:
-                data = file.data.loc[file.data[file.frame_header] == frame]
-                p = np.array([data[file.x_header],data[file.y_header]])
-                D = np.array(data[file.size_header])
-                frameData[frame][file.name] = Blobs2D(p,D)
-    return frameData
-
+    for i in range(frame_start,frame_end):
+        for file in blob_files:
+            if i in file.data[file.frame_header]:
+                data = file.data.loc[file.data[file.frame_header] == i]
+                points = np.array([data[file.x_header],data[file.y_header]])
+                diameters = np.array(data[file.size_header])
+                frame_dict[i][file.name] = BlobsFrame(points, diameters)
+    return frame_dict
 
--- test/motiontrack/test_read_file.py
+++ test/motiontrack/test_read_file.py
@@ -1,7 +1,8 @@
-import numpy as np
+""" Test module for reading blob data file and processing to a dictionary
+of frames """
 
-from motiontrack.read_data import *
+from motiontrack.read_data import read_blob_data
 
-file = 'data/example_data.txt'
-frames = read_files([file])
+FILENAMES = ['data/example_data.txt']
+frames = read_blob_data(FILENAMES)
 print(frames)
