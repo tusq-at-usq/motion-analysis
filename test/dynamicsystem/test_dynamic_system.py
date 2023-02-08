22b8d692c98ad5b2ce9af0e8fc990a74ef4a57d6
andrewjlock <andrew.john.lock@gmail.com>
2023-02-08 14:48:12 +1000

    housekeeping tests

--- test/dynamicsystem/test_dynamic_system.py
+++ test/dynamicsystem/test_dynamic_system.py
@@ -2,33 +2,33 @@
 """
 
 import pickle
-from matplotlib import pyplot as plt
+import numpy as np
+import os
+#  from matplotlib import pyplot as plt
 
-CONFIG = 'data/projectile_config'
-FILE  = 'data/projectile_system'
+CONFIG = os.path.dirname(__file__)+'/data/projectile_config'
+FILE  = os.path.dirname(__file__)+'/data/projectile_system'
 
-with open(FILE,'rb') as file:
-    dsys = pickle.load(file)
+def test_dynamic_system():
 
-#  dsys.write_default_config('test')
-dsys.load_config(CONFIG)
-dsys.add_aug_states(CONFIG)
-dsys.sub_params(CONFIG)
-dsys.J = dsys._create_jacobian()
-dsys.lambdify()
+    with open(FILE,'rb') as file:
+        dsys = pickle.load(file)
 
-x_0 = dsys.load_x_0(CONFIG)
-x_history, t_history = dsys.integrate(2.8,x_0,dt_max=0.1)
-print("Integrated state history:", x_history)
-print("Time history: ", t_history)
+    dsys.write_default_config('test')
+    os.remove("test.yaml")
+    dsys.load_config(CONFIG)
+    dsys.add_aug_states(CONFIG)
+    dsys.sub_params(CONFIG)
+    dsys.J = dsys._create_jacobian()
+    dsys.lambdify()
 
+    x_0 = dsys.load_x_0(CONFIG)
+    x_history, t_history = dsys.integrate(2.8,x_0,dt_max=0.1)
+    x_last = np.array([24.30034925, 5.66086674,
+                       1.05025819, 4.51467575,
+                      -4.91091348, -23.02609882])
 
-ax = plt.axes(projection='3d')
-ax.plot(x_history[:,0], x_history[:,1], x_history[:,2],color='k')
-ax.scatter3D(x_history[:,0], x_history[:,1], x_history[:,2], c=t_history)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-plt.show()
+    np.testing.assert_array_almost_equal(x_history[-1],x_last)
 
-
+if __name__=="__main__":
+    test_dynamic_system()
